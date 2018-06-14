116e3dcbd097440834f46ae457369c6bd9e30f66
Tyler <auslitztyler@gmail.com>
2018-06-14 14:19:56 -0400

    First

---
+++ Student/Controllers/StudentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Student.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Student model)
+        {
+            ViewBag.Message = model.FirstName + " " + model.LastName + " is the name";
+            return View();
+        }
+    }
+}
